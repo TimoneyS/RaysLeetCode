f4e774ae3f17a63ae372b3a4fe09badaf7332cfd
rays1990@163.com <rays1990@163.com>
2020-04-14 22:26:42 +0800

    leetcode

---
+++ src/com/ray/util/Out.java
@@ -0,0 +1,69 @@
+package com.ray.util;
+
+import java.util.Arrays;
+
+/**
+ * 自定义输出流辅助类
+ * @author Ray
+ *
+ */
+public class Out {
+    
+    /** 输出并换行 */ public static <T> void p() { System.out.println(); }
+    /** 输出并换行 */ public static <T> void p(T t) { System.out.println(t); }
+    /** 输出不换行 */ public static <T> void pt(T t) { System.out.print(t); }
+    /** 格式化输出 */ public static <T> void pf(String s, Object...args){ System.out.printf(s, args); }
+    /** 输出间隔符 */ public static     void sep() { sep(100); }
+    
+    /**
+     * 输出间隔符
+     * @param size
+     */
+    public static void sep(int size) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size; i++) sb.append("=");
+        p(sb.toString());
+    }
+
+    /********************************** 数组相关方法 **************************************/
+    /** 打印数组 */public static <T> void p(      T[] t) {p(Arrays.toString(t));}
+    /** 打印数组 */public static void     p(   byte[] t) {p(Arrays.toString(t));}
+    /** 打印数组 */public static void     p(  short[] t) {p(Arrays.toString(t));}
+    /** 打印数组 */public static void     p(    int[] t) {p(Arrays.toString(t));}
+    /** 打印数组 */public static void     p(   long[] t) {p(Arrays.toString(t));}
+    /** 打印数组 */public static void     p(  float[] t) {p(Arrays.toString(t));}
+    /** 打印数组 */public static void     p( double[] t) {p(Arrays.toString(t));}
+    /** 打印数组 */public static void     p(boolean[] t) {p(Arrays.toString(t));}
+    /** 打印数组 */public static void     p(   char[] t) {p(Arrays.toString(t));}
+
+    /** 打印数组 */public static <T> void p(      T[] t, String fmt) { for (T       i : t) pf(fmt, i); p();}
+    /** 打印数组 */public static void     p(   byte[] t, String fmt) { for (byte    i : t) pf(fmt, i); p();}
+    /** 打印数组 */public static void     p(  short[] t, String fmt) { for (short   i : t) pf(fmt, i); p();}
+    /** 打印数组 */public static void     p(    int[] t, String fmt) { for (int     i : t) pf(fmt, i); p();}
+    /** 打印数组 */public static void     p(   long[] t, String fmt) { for (long    i : t) pf(fmt, i); p();}
+    /** 打印数组 */public static void     p(  float[] t, String fmt) { for (float   i : t) pf(fmt, i); p();}
+    /** 打印数组 */public static void     p( double[] t, String fmt) { for (double  i : t) pf(fmt, i); p();}
+    /** 打印数组 */public static void     p(boolean[] t, String fmt) { for (boolean i : t) pf(fmt, i); p();}
+    /** 打印数组 */public static void     p(   char[] t, String fmt) { for (char    i : t) pf(fmt, i); p();}
+    
+    /** 打印二维数组 */ public static <T> void p(      T[][] arr, String fmt) { for (      T[] is : arr) p (is, fmt);}
+    /** 打印二维数组 */ public static <T> void p(   byte[][] arr, String fmt) { for (   byte[] is : arr) p (is, fmt);}
+    /** 打印二维数组 */ public static <T> void p(  short[][] arr, String fmt) { for (  short[] is : arr) p (is, fmt);}
+    /** 打印二维数组 */ public static <T> void p(    int[][] arr, String fmt) { for (    int[] is : arr) p (is, fmt);}
+    /** 打印二维数组 */ public static <T> void p(   long[][] arr, String fmt) { for (   long[] is : arr) p (is, fmt);}
+    /** 打印二维数组 */ public static <T> void p(  float[][] arr, String fmt) { for (  float[] is : arr) p (is, fmt);}
+    /** 打印二维数组 */ public static <T> void p( double[][] arr, String fmt) { for ( double[] is : arr) p (is, fmt);}
+    /** 打印二维数组 */ public static <T> void p(boolean[][] arr, String fmt) { for (boolean[] is : arr) p (is, fmt);}
+    /** 打印二维数组 */ public static <T> void p(   char[][] arr, String fmt) { for (   char[] is : arr) p (is, fmt);}
+
+    /**
+     * 打印二维数组
+     */
+    public static void p(boolean[][] arr) {
+        for (boolean[] is : arr) {
+            for (boolean i : is)
+                Out.pt(i? "T " : "F ");
+            Out.p();
+        }
+    }
+}
---
+++ src/com/ray/util/Timer.java
@@ -0,0 +1,90 @@
+package com.ray.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 计时器类
+ * @author Ray
+ *
+ */
+public class Timer {
+	
+    private static SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat("[yyyy-MM-dd hh:mm:ss:ms]");
+	
+	private String name;
+    private int record_count = 0;
+    private long[] dateArr = new long[16];
+
+    private static final Timer DEFAULT = create("TIMER");
+    
+    public static void CLICK() {
+        DEFAULT.click();
+    }
+    
+    public static void STOP() {
+        DEFAULT.stop();
+    }
+    
+    
+    /**
+	 * 时间戳
+	 */
+	public static String tag() {
+        return DATE_FORMATTER.format(new Date());
+	}
+	
+	public static Timer create(String name) {
+	    return new Timer(name);
+	}
+	
+	private Timer(String name) {
+	    if (name == null) name = "";
+	    this.name = name;
+    }
+	
+	/**
+     * "按下" 计时器<br/>
+     * 第一次按下 开始记录时间，之后每次按下将显示距离上一次的时间，并开始下一次计时
+     */
+	public void click() {
+        adjustDateArray();
+        dateArr[record_count ++] = new Date().getTime();
+	}
+	
+    /**
+     * "停止" 计时器
+     */
+    public void stop() {
+        click();
+        show();
+        record_count = 0;
+    }
+    
+    /**
+     * "显示" 计时器结果
+     */
+    public void show() {
+       for (int i = 1; i < record_count; i++) {
+           Out.pf("[%s]第%2d次耗时 %s ms\n", name, i, dateArr[i] - dateArr[i-1]);
+       } 
+    }
+    
+    /**
+     * 调整数组
+     */
+    private void adjustDateArray() {
+        if (record_count+1 >= dateArr.length) {
+            long[] temp = new long[dateArr.length * 2];
+            for (int i = 0; i < dateArr.length; i++) {
+                temp[i] = dateArr[i];
+            }
+            dateArr = temp;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Timer.CLICK();
+    }
+	   
+}
