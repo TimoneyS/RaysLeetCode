2c5089111f9df2ecd89b299994354eb21ab7109e
rays1990@163.com <rays1990@163.com>
2020-11-24 23:38:55 +0800

    修改层级

--- src/com/ray/leetcode/resolved/L0001_Two_Sum.java
+++ src/com/ray/leetcode/resolved/L0001_Two_Sum.java
@@ -6,17 +6,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Two Sum
- * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
- * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * 1.两数之和
+ * ==========================================
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，
+ * 并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
  * <p>
- *
  * Example:
- * Given nums = [2, 7, 11, 15], target = 9,
- * Because nums[0] + nums[1] = 2 + 7 = 9,
- * return [0, 1].
- *
- * level : Easy
+ * 示例 1
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ * <p>
+ * Level : Easy
  *
  * @author ray
  * @link https://leetcode-cn.com/problems/two-sum/
@@ -24,18 +26,18 @@ import java.util.Map;
  */
 public class L0001_Two_Sum {
     /**
-     * 如果 a + b = target
-     * 则    a = target - b
-     *
-     * 如果知道了 a， 那么只要在数组中寻找 target - b 即可。
-     * 可以构建一个数组值到坐标的映射，然后对于每个 a 寻找否存在 target - b 到坐标的映射
+     * b = target - aW
+     * 如果知道了 a， 那么只要在数组中寻找 target - a 即可。
+     * 可以构建一个数组值到坐标的映射，然后对于每个 a 寻找否存在 target - a 到坐标的映射
      */
     static class Solution {
-        public int[] twoSum(int[] numbers, int target) {
+        public int[] twoSum(int[] nums, int target) {
             Map<Integer, Integer> remain = new HashMap<>();
-            for (int i = 0; i < numbers.length; i++) {
-                if (remain.containsKey(numbers[i]))  return new int[]{remain.get(numbers[i]), i};
-                remain.put(target-numbers[i], i);
+            for (int i = 0; i < nums.length; i++) {
+                if (remain.containsKey(nums[i])) {
+                    return new int[]{remain.get(nums[i]), i};
+                }
+                remain.put(target - nums[i], i);
             }
             return new int[]{-1, -1};
         }
