9182117e7ec97e13cda9773e00ebd3ebb747f482
rays1990@163.com <rays1990@163.com>
2020-12-11 00:25:39 +0800

    提交答案

--- src/com/ray/leetcode/trans/L0001_Two_Sum.java
+++ src/com/ray/leetcode/trans/L0001_Two_Sum.java
@@ -1,4 +1,4 @@
-package com.ray.leetcode.resolved;
+package com.ray.leetcode.trans;
 
 import com.ray.util.Out;
 
@@ -8,21 +8,20 @@ import java.util.Map;
 /**
  * 1.两数之和
  * ==========================================
- * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，
- * 并返回他们的数组下标。
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
  * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
- * <p>
+ *
  * Example:
- * 示例 1
- * 给定 nums = [2, 7, 11, 15], target = 9
- * 因为 nums[0] + nums[1] = 2 + 7 = 9
- * 所以返回 [0, 1]
- * <p>
+ *      示例 1
+ *      给定 nums = [2, 7, 11, 15], target = 9
+ *      因为 nums[0] + nums[1] = 2 + 7 = 9
+ *      所以返回 [0, 1]
+ *
  * Level : Easy
  *
- * @author ray
- * @link https://leetcode-cn.com/problems/two-sum/
- * @since 2020-02-13 17:46:36
+ * @author  ray
+ * @link    https://leetcode-cn.com/problems/two-sum/
+ * @since   2020-02-13 17:46:36
  */
 public class L0001_Two_Sum {
     /**
